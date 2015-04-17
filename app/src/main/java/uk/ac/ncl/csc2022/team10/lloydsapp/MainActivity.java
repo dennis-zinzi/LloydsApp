6642a7ab4c01b5310c5766750898da3355fbd89f
RhysCovellUni <r.covell@ncl.ac.uk>
2015-04-17 04:29:47 +0100

    Budget { Someone take a look at setting the time periods, I couldn't think of a way to do it. }
    Balance page updated {
    The number appearing below the progress bar is incorrect.
    In progres.
    Points & EligableForPrizeDraw to be added.
    }

--- app/src/main/java/uk/ac/ncl/csc2022/team10/lloydsapp/MainActivity.java
+++ app/src/main/java/uk/ac/ncl/csc2022/team10/lloydsapp/MainActivity.java
@@ -137,6 +137,14 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
     }
 
     @Override
+    public void onRestart()
+    {
+        super.onRestart();
+        finish();
+        startActivity(getIntent());
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
