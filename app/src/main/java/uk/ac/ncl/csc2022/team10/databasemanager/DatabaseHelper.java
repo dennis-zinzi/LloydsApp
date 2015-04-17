1b9e9df82cdc377a654bd2122b7638b0fb8572e8
szholdiyarov <s.zholdiyarov@newcastle.ac.uk>
2015-04-17 14:56:46 +0100

    little changes

--- app/src/main/java/uk/ac/ncl/csc2022/team10/databasemanager/DatabaseHelper.java
+++ app/src/main/java/uk/ac/ncl/csc2022/team10/databasemanager/DatabaseHelper.java
@@ -2,7 +2,7 @@ package uk.ac.ncl.csc2022.team10.databasemanager;
 
 /**
  * Created by szholdiyarov on 4/17/15.
- * Purpose: Database Helper to be used appropriately.
+ * Purpose: Database Helper to be used by my colleagues.
  */
 
 import java.io.DataOutputStream;
@@ -57,5 +57,19 @@ public class DatabaseHelper {
         wr.close();
     }
 
+    public void createGame(String incompetition, String points, String user_id) throws Exception {
+        String url = "http://sprs1994.co.uk/createcontact.php";
+        URL obj = new URL(url);
+        HttpURLConnection con1 = (HttpURLConnection) obj.openConnection();
+        con1.setRequestMethod("POST");
+        con1.setRequestProperty("User-Agent", USER_AGENT);
+        con1.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        String urlParameters = "incompetition=" + incompetition + "&points=" + points + "&user_id=" + user_id;
+        con1.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con1.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+    }
 
 }
--- app/src/main/java/uk/ac/ncl/csc2022/team10/databasemanager/DatabaseManager.java
+++ app/src/main/java/uk/ac/ncl/csc2022/team10/databasemanager/DatabaseManager.java
@@ -68,4 +68,6 @@ public class DatabaseManager {
         }
         return 0;
     }
+
+
 }
