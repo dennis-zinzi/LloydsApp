ee93fbe80d3d96227c5fcdc6f7db8c41e50112bb
RhysCovellUni <r.covell@ncl.ac.uk>
2015-04-17 05:37:19 +0100

    Balance page update.

--- app/src/main/java/uk/ac/ncl/csc2022/team10/lloydsapp/BalanceFragment.java
+++ app/src/main/java/uk/ac/ncl/csc2022/team10/lloydsapp/BalanceFragment.java
@@ -1,27 +1,21 @@
 package uk.ac.ncl.csc2022.team10.lloydsapp;
 
 import uk.ac.ncl.csc2022.team10.datatypes.*;
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.graphics.Color;
+import uk.ac.ncl.csc2022.team10.budgeting.BudgetActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.text.InputType;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -31,7 +25,7 @@ public class BalanceFragment extends Fragment {
 
     private RelativeLayout myLinearLayout;
     private LinearLayout budgetLin;
-    private TextView budgetLabel1;
+    private TextView pointsComp;
     private TextView balanceLabel;
     private TextView overdraftLabel;
     private Button manageBudgets;
@@ -60,9 +54,12 @@ public class BalanceFragment extends Fragment {
         overdraftLabel = (TextView) rootView.findViewById(R.id.overdraftText);
         budgetLin = (LinearLayout) rootView.findViewById(R.id.exteriorBudgets);
         manageBudgets = (Button) rootView.findViewById(R.id.budgetManage);
+        pointsComp = (TextView) rootView.findViewById(R.id.pointsSection);
 
         balanceLabel.setText(String.format("%.2f", user.getAccount().getBalance()));
         overdraftLabel.setText(String.format("%.2f", user.getAccount().getOverdraftLimit()));
+        pointsComp.setText("Points: "+user.getPoints().toString()
+                        +"\nEntered into competition: "+ false); // ---------------------------Need boolean for whether entered or not.
 
 
         manageBudgets.setOnClickListener(new OnClickListener() {
@@ -103,25 +100,25 @@ private void previewBudgets(){
     int netProfOrLoss=0;
     int sumLimits=0;
     int sumCurrentSpend=0;
-    for (int i = 0; i < budgets.size(); i++) {
+    for (int i = 0; i < budgets.size(); i++)
+    {
         sumLimits += ((int) (budgets.get(i).getLimit()));
-        sumCurrentSpend+= ((int) budgets.get(i).getCurrentSpend());
-        netProfOrLoss+=sumLimits-sumCurrentSpend;
+        sumCurrentSpend += ((int) budgets.get(i).getCurrentSpend());
     }
+    netProfOrLoss+=(sumLimits-sumCurrentSpend);
 
     try {
         pgPreview.setProgress(sumCurrentSpend * 100 / sumLimits);
+
     }catch (ArithmeticException e){
         pgPreview.setProgress(0);
-        pgPreview.setContentDescription("No Budgets");
-
     }
 
     if (netProfOrLoss<0){
-        summaryPrev.setText("Spent -"+Math.abs(netProfOrLoss));
+        summaryPrev.setText("Not Saved -£"+Math.abs(netProfOrLoss));
     }else
     {
-        summaryPrev.setText("Saved +"+netProfOrLoss);
+        summaryPrev.setText("Saved £"+netProfOrLoss);
     }
     }
 }
