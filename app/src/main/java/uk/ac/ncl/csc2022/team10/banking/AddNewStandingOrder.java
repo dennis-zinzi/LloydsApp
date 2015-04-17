7f1faa6cca4e2c750ac72eedd99e7730999ad763
szholdiyarov <s.zholdiyarov@newcastle.ac.uk>
2015-04-17 11:33:10 +0100

    Add new standing order

---
+++ app/src/main/java/uk/ac/ncl/csc2022/team10/banking/AddNewStandingOrder.java
@@ -0,0 +1,43 @@
+package uk.ac.ncl.csc2022.team10.banking;
+/* Created by szholdiyarov
+ * Purpose: Activity to add new standing orders
+ */
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import uk.ac.ncl.csc2022.team10.lloydsapp.R;
+
+public class AddNewStandingOrder extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_standing_order);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_new_standing_order, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
